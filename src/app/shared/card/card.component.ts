e175cf84103276c950c85d30da85fdffc0f62e7e
rafaelcesar0 <rafaelcesar0@proton.me>
2025-02-08 15:14:26 -0300

    style: card.component

--- src/app/shared/card/card.component.ts
+++ src/app/shared/card/card.component.ts
@@ -4,20 +4,8 @@ import { Component } from '@angular/core';
   selector: 'app-card',
   standalone: true,
   imports: [],
-  template: `
-    <div>
-      <ng-content />
-    </div>
-  `,
-  styles: `
-  div {
-    border-radius: 6px;
-    box-shadow: 0 1px 6px rgba(0, 0, 0, 0.1);
-    overflow: hidden;
-  }
-  `,
-  // templateUrl: './card.component.html',
-  // styleUrl: './card.component.scss'
+  templateUrl: './card.component.html',
+  styleUrl: './card.component.scss'
 })
 export class CardComponent {
 
